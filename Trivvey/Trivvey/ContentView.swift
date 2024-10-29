16949c58be74e21751b5d27fc6efb032f27948bf
Brenton Lian <blian@unc.edu>
2024-10-29 19:12:37 -0400

    Final commit

--- Trivvey/Trivvey/ContentView.swift
+++ Trivvey/Trivvey/ContentView.swift
@@ -2,20 +2,40 @@
 //  ContentView.swift
 //  Trivvey
 //
-//  Created by Alexandra Marum on 10/21/24.
+//  Created by Brenton Lian on 10/28/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var questions = [Question]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(questions, id: \.question) { question in
+                NavigationLink(question.question, destination: QuestionView(question: question))
+            }
+            .navigationTitle("Trivvey")
+            .onAppear {
+                let url: URL = Bundle.main.url(forResource: "questions", withExtension: "json")!
+                let data = try! Data(contentsOf: url)
+                questions = try! JSONDecoder().decode([Question].self, from: data)
+                questions.shuffle()
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        questions.shuffle()
+                    } label: {
+                        HStack {
+                            Image(systemName: "shuffle")
+                            Text("Shuffle")
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
-        .padding()
     }
 }
 
--- Trivvey/Trivvey/Question.swift
+++ Trivvey/Trivvey/Question.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Question: Codable {
+        let question: String
+        let A: String
+        let B: String
+        let C: String
+        let D: String
+        let answer: String
+    
+        static let example = Question(
+            question: "What is the capital of France?",
+            A: "Berlin",
+            B: "Madrid",
+            C: "Paris",
+            D: "Rome",
+            answer: "C"
+        )
+}
+
+
+
